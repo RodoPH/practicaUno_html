f091904cdb95212e6d18695fabef348e64423b6c
RodoPH <152t0191@itsm.edu.mx>
2019-03-12 12:40:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Primer práctica</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<img id="imgTec" src="http://www.itsm.edu.mx/images/enc_itsm.png" alt="">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="estilos.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/pro.js"></script>
+</head>
+<!--//////////////////////BODY///////////////-->
+<body>
+	<script type="text/javascript">
+window.onload=function(){alert('Bienvenido a esta pagina');}
+window.onunload=function(){alert('Vuelva en otro momento');}
+</script>
+	<h3><p>Residencias profesionales </h3>
+	<br>La Residencia Profesional es una estrategia educativa, que permite al estudiante incorporarse profesionalmente, antes de egresar, al entorno laboral a través del desarrollo de proyectos, definidos entre la institución educativa y la empresa. La Residencia Profesional se realiza con apoyo de asesorías académico - empresariales. </p>
+
+	<ol>
+		<li><a id="objetivo" href="#primero">Obejivos</a></li>
+		<li><a href="#segundo">Opciones para la realización de residencias profesionales</a></li>
+		<li><a href="#tercer">Requisitos para registrar las residencias profesionales</a></li>
+		<li><a href="#cuarto">Reinscripción al semestre ante servicios escolares</a></li>
+		<li><a href="#quinto">Asesores de residencias profesionales</a></li>
+		<li><a href="#sexto">Reporte final</a></li>
+		<li> <a href="#octa">Preguntas frecuentes</a></li>
+		<li> <a href="#nove">Descargar archivos</a></li>
+	<br><br>
+
+		<iframe align="center" width="200" height="150" src="https://www.youtube.com/embed/0iLF_rtUbq0?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	
+	</ol>
+	<br>
+	<ul>
+	<h2><p id="primero"><a href="#">Obejivos</a> <br> </h2>
+		<ul>
+			<h4><li>Para la empresa</li></h4>
+			<ul><li>Coadyuvar en la formación cualitativa y cuantitativa de los alumnos.</li><li>Beneficiar a los participantes con la elaboración de proyectos actuales.</li><li>Planear el desarrollo de los recursos humanos de los participantes.</li><br></ul>
+			<br>
+			<h4><li>Para el tecnológico</li></h4>
+			<ul>
+				<li>Formar profesionistas acordes a los requerimientos de su entorno y al perfil profesional de las carreras que oferta, cumpliendo así con su misión.</li>
+				
+
+			</ul>
+			<h4><li>Para el(la) alumno(a)</li></h4>
+			<ul>Integrarse a la problemática laboral y empresarial a la que se enfrentará en el ejercicio de su profesión aportando su mejor esfuerzo y sus conocimiento con una mentalidad creativa que le conduzca al éxito en su realización personal, profesional y económica.</li>
+					
+
+			</ul>
+
+		</ul>
+	</p>
+
+	<p id="segundo"><h2><a href="#">Opciones para la realización de residencias profesionales</a></h2><br>
+	
+		<ul><h4><li>Las Residencias Profesionales pueden realizarse por tres opciones: </li></h4> 
+			<ul>Integrarse a la problemática laboral y empresarial a la que se enfrentará en el ejercicio de su profesión aportando su mejor esfuerzo y sus conocimiento con una mentalidad creativa que le conduzca al éxito en su realización personal, profesional y económica.</li>
+				<ul>
+					<li>Banco de proyectos.</li>
+					<li>Por planteamiento de proyecto propio.</li>
+					<li>Por situación laboral en una empresa.</li>
+
+				</ul>
+			</ul>	
+				</ul>
+			</p
+			>
+
+	<li><p id="tercer"><h2><a href="#">Requisitos para registrar las residencias profesionales</a></h2></li><br>
+
+		<ol>
+			<li>Llenar el Formato de Carta de Asignación de Residencia Profesional <a href="http://www.itsm.edu.mx/comunidad%20estudiantil/residencias/F-SA-08%20%20Carta%20de%20Asignacion%20de%20Residencia%20Profesional%20Actual.doc">(Formato FSA-08)</a></li>
+			<li>Entregar anteproyecto escrito a computadora, en papel tamaño carta color blanco, a espacio y medio, tipo de letra ARIAL 10 con el siguiente contenido:</li>
+			<ul>
+				<li> Nombre y Objetivo del proyecto.</li>
+				<li>Justificación. </li>
+				<li>Cronograma preliminar de actividades.</li>
+				<li> Descripción detallada de las actividades.</li>
+				<li>Lugar donde se realizará el proyecto.</li>
+				<li>Información sobre la empresa, organismo o dependencia para la que se desarrollará el proyecto.</li>
+			</ul>
+			<li>Copia de la Carta de Aceptación de la Empresa ó Institución donde se llevará a cabo la Residencia Profesional (formato libre).</li>
+			<li>Kardex del cumplimiento de 75 % de créditos aprobados.</li>
+		</ol>
+	
+	<p id="cuarto"><li><h2><a href="#">Reinscripción al semestre ante servicios escolares</a></h2></li><br>
+		<ol>
+			<li>Arancel de pago correspondiente de reinscripción.</li>
+			<li>Formato de solicitud de carga académica autorizado por la (el) Jefa (e) de Carrera.</li>
+			<li>Lo designa e informa al residente. El alumno residente deberá entregar por lo menos cuatro reportes (Formato FSA-29).</li>
+		</ol>
+
+	<p id="quinto">	<li><h2><a href="#">Asesores</a></h2></li>
+			<ul>
+				<li><strong>Asesor interno</strong> Es un (a) docente de la carrera que supervisará y guiará el proyecto, y es designado por la (el) Jefa (e) de Carrera.</li>
+				<li><strong>Asesor externo</strong> Responsable del proyecto por parte de la empresa o institución, autoriza los informes (Formato FSA-29) por medio de firma y sello. </li>
+			</ul>
+	</p>
+
+	<p id="sexto">	<li><h2><a href="#">Reporte final</a></h2></li>
+			<ul>
+				<li>Será requisito para la acreditación de la Residencia Profesional, la elaboración de un informe final del proyecto realizado.</li>
+				<li>El reporte deberá entregarse en formato PDF y en CD al jefe de carrera en la fecha establecida.</li>
+			</ul>
+		</p>
+	<p id="septimo">	<li><h2><a href="#">Puntos que debe cubrir el reporte final</a></h2></li>
+		<ul>
+					<strong>El documento deberá estructurarse incluyendo los siguientes contenidos:  </strong>
+					<ul>
+					<li>Portada.</li>
+					<li>Justificación</li>
+					<li>Objetivos</li>
+					<li>Problemas a resolver, priorizándolos.</li>
+					<li>Procedimiento y descripción de las actividades realizadas</li>
+					<li>Resultados, planos, gráficas, prototipos, maquetas, programas, entre otros.</li>
+					<li>Conclusiones y recomendaciones.</li>
+					<li>Conclusiones y recomendaciones.</li>
+					<li>Competencias desarrolladas y/o aplicacas.</li>
+					<li>Referencias bibliográficas y virtuales</li>
+					</ul>
+				</ul>
+	</p>
+
+	<p id="octa">	<li><h2><a href="#">Preguntas frecuentes</a></h2></li>
+	<ul>
+		<li><strong>¿Cómo consigo el Formato del Reporte Mensual?</strong><br>
+				El formato puedes descargarlo aquí. <br></li>
+		
+		<li><strong>¿Cuántos reportes debo entregar?</strong><br>
+		Cuatro reportes mensuales (Formato FSA-29), uno al asesor con copia al jefe de carrera</li>
+		
+		<li><strong>¿Qué hago si la empresa me cambia de proyecto?</strong><br>
+		Notifica inmediatamente al jefe de carrera y asesor para hacer el cambio correspondiente y puedas continuar con el nuevo proyecto. <br></li>
+		
+		<li><strong>¿Se puede cambiar el nombre del proyecto?</strong><br>
+		Si, justifica el cambio ante el jefe de carrera y asesor, debes hacerlo en los primeros dos meses para que se realice el cambio en servicios escolares. <br></li>
+
+		<li><strong>¿Qué hago si mi asesor no me contesta?</strong><br> 
+		Escribe un email o habla por teléfono inmediatamente al jefe de carrera.
+		<br></li>
+		
+		<li><strong>¿Puedo cancelar la residencia?</strong> <br>
+		Sí, siempre y cuando exista una causa y evidencia que justifique la cancelación, envía un oficio al jefe de carrera explicando ampliamente la causa. La (El) Jefa(e) de Carrera te indicará el procedimiento a realizar. 
+		<br></li>
+	
+		<li><strong>¿Qué pasa si no entrego el reporte final?</strong><br>
+		El reporte final es el último paso para concluir las residencias, si no se entrega no hay calificación y se considera una asignatura reprobada.
+		<br></li>
+	
+		<li><strong>¿Puedo iniciar la residencia profesional antes de inscribirme?</strong><br>
+		La residencia profesional es una asignatura de tu plan de estudios, debes inscribirte y cumplir con los tiempos establecidos. Otra situación consultarlo con la (el) jefa (e) de carrera.
+		<br></li>
+
+		<li><strong>¿Puedo hacer servicio social y residencia a la vez?</strong><br>
+		Sí, solo recuerda que cada uno tiene diferente normatividad. Consúltala en esta pagina web, sección "Comunidad estudiantil" en el apartado Normatividades.
+		<br></li>
+		
+		<li><strong>¿Cuántos estudiantes pueden estar en un proyecto de residencia profesional?</strong><br>
+		El número es variable siempre y cuando el proyecto lo justifique.
+		<br></li>
+		
+		<li><strong>¿Qué pasa si me enfermo y no puedo concluir las residencias profesionales?</strong><br>
+		Puedes cancelarla, enviando una solicitud al(a) jefe (a) de carrera, con copia del certificado médico, para que este sea sometido a comité académico.
+		<br></li>
+	
+		<li><strong>¿Tengo que entregar los reportes mensuales personalmente?</strong><br>
+		No, digitalízalos y envíalos al e-mail a tu asesor(a) y copia al(a) jefe(a) de carrera.
+		<br></li>
+	
+	</ul>
+	</p>	
+	
+	<p id="nove"><li><h2><a href="#">Descargar archivos</a></h2></li>
+
+	<ul>
+		<li><a href="http://www.itsm.edu.mx/comunidad%20estudiantil/residencias/F-SA-29%20Reporte%20mensual%20d%20residencia%20profesional%20Rev00.doc">Reporte mensual de residencias.</a></li>
+		<li><a href="http://www.itsm.edu.mx/comunidad%20estudiantil/residencias/Caracteristicas%20de%20presentacion%20del%20reporte%20final%202016.pdf" target="_blank" onclick="javascript:alert('Saliendo');">Características de presentación del reporte final</a></li>
+	</ul>
+	</p>
+	</ul>
+</body>
+</html>
